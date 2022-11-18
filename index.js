4d93c6f13e4c840710f7b6362cdd42be2f1323a0
andrey789-c <andreymak2004@gmail.com>
2022-11-18 10:44:54 +1000

    slider

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ const offsetWidth = window.outerWidth
 
 slider.addEventListener('mousemove', e => {
     if(window.innerWidth > 768) {
-        let x = e.x / (slider.scrollWidth) * 100
+        let x = (width / 2 - e.x) / (slider.scrollWidth) * 100
         if(e.x <= width / 2) {
-            let width2 = e.x - width /2
+            let width2 = e.x - width
             let x2 = width2 / slider.scrollWidth * 100
-            console.log(x2);
-            sliderItems.style.transform =`translateX(${-x2 * 3.5}%)`
+            if(sliderItems.getBoundingClientRect().x < 0) {
+                sliderItems.style.transform =`translateX(0)`
+            } else {
+                sliderItems.style.transform =`translateX(${-x2}%)`
+
+            }
         } else {
-            sliderItems.style.transform = `translateX(-${x * 0.9}%)`
+            sliderItems.style.transform = `translateX(${x}%)`
         }
     }
     
@@ -82,4 +86,11 @@ window.addEventListener('resize', e => {
     }
 
     
+})
+
+let widthImg = document.querySelectorAll('.slider__img')[0].offsetWidth
+
+document.querySelectorAll('.slider__img').forEach(img => {
+    img.style.width = widthImg
+    img.style.height = widthImg + 100 / 0.72 +'px'
 })
